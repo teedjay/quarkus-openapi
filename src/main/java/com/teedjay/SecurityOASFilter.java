63ff2fd7d4ace9df80ddcb84eb5213cd3043fbac
Thore Johnsen <thore.johnsen@evry.com>
2020-11-03 16:40:47 +0100

    Simplifed OAS filter by looking up properties "manually".

--- src/main/java/com/teedjay/SecurityOASFilter.java
+++ src/main/java/com/teedjay/SecurityOASFilter.java
@@ -1,21 +1,25 @@
 package com.teedjay;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.openapi.OASFilter;
 import org.eclipse.microprofile.openapi.models.security.OAuthFlow;
 import org.eclipse.microprofile.openapi.models.security.SecurityScheme;
 
 public class SecurityOASFilter implements OASFilter {
 
-    public static String url = "not-set";
-
     @Override
     public SecurityScheme filterSecurityScheme(SecurityScheme securityScheme) {
         if (securityScheme.getType().equals(SecurityScheme.Type.OAUTH2)) {
+            String url = authorizationUrl();
             System.out.printf("Changing authorizationUrl for '%s' to : %s%n", securityScheme.getType(), url);
             OAuthFlow flow = securityScheme.getFlows().getImplicit();
             flow.setAuthorizationUrl(url);
         }
         return securityScheme;
+    }
+
+    private String authorizationUrl() {
+        return ConfigProvider.getConfig().getValue("my.authorization.url", String.class);
     }
     
 }
